19e1082cf404b8300ac0b49c744b0c880ee0891c
hnx98 <57165312+hnx98@users.noreply.github.com>
2019-10-30 00:13:00 -0400

    Add files via upload
    stream

---
+++ Test.java
@@ -0,0 +1,20 @@
+package myJava;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] arg){
+
+		List<Integer> lNum = Arrays.asList(3,5,10,20,-30);
+		lNum.stream().filter(num -> num % 5 == 0).forEach(System.out::println);
+		lNum
+		  .stream()
+		  .filter(num -> num > 4 && num < 0)
+		  .forEach(System.out::println);
+
+	}
+	
+	
+}
